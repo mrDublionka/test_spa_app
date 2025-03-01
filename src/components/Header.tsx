102154e40a47ab6bf14f0637017a6fb972510dd1
Ion Cojocari <69515189+Cojocari-Ion@users.noreply.github.com>
2025-03-01 22:26:39 +0200

    General settings. Fonts, color variables, seize variables, breakpoints.

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import st from "@/styles/Header.module.scss"
+
+const Header = () => {
+    return (
+        <div className={st.header}>
+            <div className={st.header__left}>
+
+            </div>
+
+            <div className={st.header__right}>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/MainComponent.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header.tsx";
+
+
+const MainComponent = () => {
+
+    return (
+        <main >
+            <Header />
+
+        </main>
+    )
+}
+
+export default MainComponent
