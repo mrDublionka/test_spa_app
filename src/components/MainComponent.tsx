4f9213be0354ecc162370d67fb806637106c3763
Ion Cojocari <69515189+Cojocari-Ion@users.noreply.github.com>
2025-03-05 22:36:53 +0200

    Mobile default tab (side) from local storage

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -6,11 +6,20 @@ import Body from "@/components/Body.tsx";
 
 
 const MainComponent = () => {
+    const storage_value = 'active_side'
 
-    const [active_side, set_active_side] = useState<Sides>(Sides.Products)
+    let default_active_side = localStorage.getItem(storage_value) as Sides | null || Sides.Products;
+
+    if (default_active_side !== Sides.Feedback && default_active_side !== Sides.Products) {
+        default_active_side = Sides.Products;
+    }
+
+    const [active_side, set_active_side] = useState<Sides>(default_active_side)
+
 
     const active_handler = (x:Sides) => {
         set_active_side(x)
+        localStorage.setItem(storage_value, x)
     }
 
     const common_props:CommonPropsInterface = {active_side, active_handler}
