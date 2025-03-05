d48b0cfc83b3f7bfaa49f07d745f1f07d98c26f8
Ion Cojocari <69515189+Cojocari-Ion@users.noreply.github.com>
2025-03-05 23:34:19 +0200

    Solving deprecated sass imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 // @ts-ignore
 import path from "path";
-import sass from 'sass'
-
+import sass from 'sass';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
-  resolve: {
-    alias: {
-      // @ts-ignore
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            // @ts-ignore
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // @ts-ignore
-        implementation: sass,
-      },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // @ts-ignore
+                implementation: sass,
+            },
+        },
+        modules: {
+            localsConvention: "camelCase",
+            generateScopedName: "[name]__[local]___[hash:base64:5]"
+        },
     },
-    modules: {
-      localsConvention: "camelCase",
-      generateScopedName: "[name]__[local]___[hash:base64:5]"
-    },
-  },
-})
+});
